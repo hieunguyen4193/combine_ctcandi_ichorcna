744a3dd1986f53f551e416d29701afdd8de4673d
hieunguyen4193 <work@hieunguyen.de>
2025-01-22 23:24:58 +0100

    update

--- dataset_paths.py
+++ dataset_paths.py
@@ -1,7 +1,9 @@
 dataset_paths = {
     "LOD": "/media/hieunguyen/GSHD_HN01/raw_data/reads_from_450_regions_LOD_samples",
-    "SPIKE_IN": "/media/hieunguyen/GSHD_HN01/raw_data/reads_from_450_regions_spikein_v2",
+    "SPIKE_IN_20250122": "/media/hieunguyen/GSHD_HN01/raw_data/reads_from_450_regions_SPIKE_IN_20250122",
     "REPORT4": "/media/hieunguyen/GSHD_HN01/raw_data/reads_from_450_regions_with_readname_TMDfull",
     "VALIDATION": "/media/hieunguyen/GSHD_HN01/raw_data/reads_from_450_regions_validations_Vi-Truong",
-    "CONTROL": "/media/hieunguyen/GSHD_HN01/raw_data/reads_from_450_regions_with_readname_TMDfull_control"
+    "CONTROL": "/media/hieunguyen/GSHD_HN01/raw_data/reads_from_450_regions_with_readname_TMDfull_control",
+    "ctCandi_20250122_Hanh": "/media/hieunguyen/GSHD_HN01/raw_data/ctCandi_20250122_Hanh/reads_from_450_regions",
+    "ctCandi_20250122_Vi": "/media/hieunguyen/GSHD_HN01/raw_data/ctCandi_20250122_Vi/reads_from_450_regions"
 }
